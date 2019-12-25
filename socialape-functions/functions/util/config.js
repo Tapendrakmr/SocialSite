d9cd3062caf019defda9394086fb1354c869606c
username <tapendrakmr786@gmail.com>
2019-12-25 16:23:06 +0530

    file uploading

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAr3SYXR9qT_clovxxr1tBb3o4u_jX-pdQ",
+  authDomain: "socialape-e962c.firebaseapp.com",
+  databaseURL: "https://socialape-e962c.firebaseio.com",
+  projectId: "socialape-e962c",
+  storageBucket: "socialape-e962c.appspot.com",
+  messagingSenderId: "667361709637",
+  appId: "1:667361709637:web:29a76bf940d97493e65d2c",
+  measurementId: "G-JJN0ZX5BX9"
+};
